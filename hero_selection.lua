60b8393fb01b0be51281ae22ea8615628d4a8ff5
ShadowTinker <1193656624@qq.com>
2020-01-15 17:10:40 +0800

    add heroselecting for dire

--- hero_selection.lua
+++ hero_selection.lua
@@ -1,3 +1,10 @@
 function Think()
-
+    if(GetTeam()==TEAM_DIRE)
+    then
+		SelectHero( 5, "npc_dota_hero_faceless_void" );
+		SelectHero( 6, "npc_dota_hero_tinker" );
+		SelectHero( 7, "npc_dota_hero_bristleback" );
+		SelectHero( 8, "npc_dota_hero_bounty_hunter" );
+		SelectHero( 9, "npc_dota_hero_grimstroke" );
+	end
 end
